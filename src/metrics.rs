f3c4a4f948e99e1fb9b0474fc3d9225249714f2c
Ville Vesilehto <ville@vesilehto.fi>
2024-11-05 21:10:37 +0200

    chore: add metrics labels

--- src/metrics.rs
+++ src/metrics.rs
@@ -13,9 +13,9 @@ pub struct Metrics {
     requests_total: Counter<u64>,
     requests_in_flight: UpDownCounter<i64>,
     request_duration: Histogram<f64>,
-    prom_requests_total: prometheus::Counter,
-    prom_requests_in_flight: prometheus::Gauge,
-    prom_request_duration: prometheus::Histogram,
+    prom_requests_total: prometheus::IntCounterVec,
+    prom_requests_in_flight: prometheus::IntGaugeVec,
+    prom_request_duration: prometheus::HistogramVec,
     registry: prometheus::Registry,
 }
 
@@ -30,18 +30,29 @@ impl Metrics {
         let meter = opentelemetry::global::meter("http_server");
         let registry = prometheus::Registry::new();
         
-        // Create Prometheus metrics
-        let requests_total = prometheus::Counter::new("http_requests_total", "Total number of HTTP requests").unwrap();
-        let requests_in_flight = prometheus::Gauge::new("http_requests_in_flight", "Number of HTTP requests currently in flight").unwrap();
-        let request_duration = prometheus::Histogram::with_opts(prometheus::HistogramOpts::new(
-            "http_request_duration_seconds",
-            "HTTP request duration in seconds",
-        )).unwrap();
+        // Create Prometheus metrics with labels
+        let prom_requests_total = prometheus::IntCounterVec::new(
+            prometheus::Opts::new("http_requests_total", "Total number of HTTP requests"),
+            &["method"]
+        ).unwrap();
+    
+        let prom_requests_in_flight = prometheus::IntGaugeVec::new(
+            prometheus::Opts::new("http_requests_in_flight", "Number of HTTP requests currently in flight"),
+            &["method"]
+        ).unwrap();
+    
+        let prom_request_duration = prometheus::HistogramVec::new(
+            prometheus::HistogramOpts::new(
+                "http_request_duration_seconds",
+                "HTTP request duration in seconds",
+            ),
+            &["method", "status"]
+        ).unwrap();
         
         // Register metrics with Prometheus
-        registry.register(Box::new(requests_total.clone())).unwrap();
-        registry.register(Box::new(requests_in_flight.clone())).unwrap();
-        registry.register(Box::new(request_duration.clone())).unwrap();
+        registry.register(Box::new(prom_requests_total.clone())).unwrap();
+        registry.register(Box::new(prom_requests_in_flight.clone())).unwrap();
+        registry.register(Box::new(prom_request_duration.clone())).unwrap();
 
         // Create OpenTelemetry metrics
         let otel_requests_total = meter
@@ -63,9 +74,9 @@ impl Metrics {
             requests_total: otel_requests_total,
             requests_in_flight: otel_requests_in_flight,
             request_duration: otel_request_duration,
-            prom_requests_total: requests_total,
-            prom_requests_in_flight: requests_in_flight,
-            prom_request_duration: request_duration,
+            prom_requests_total,
+            prom_requests_in_flight,
+            prom_request_duration,
             registry,
         }
     }
@@ -74,8 +85,9 @@ impl Metrics {
         let attributes = &[KeyValue::new("method", method.to_string())];
         self.requests_total.add(1, attributes);
         self.requests_in_flight.add(1, attributes);
-        self.prom_requests_total.inc();
-        self.prom_requests_in_flight.inc();
+        // Update Prometheus metrics with labels
+        self.prom_requests_total.with_label_values(&[method]).inc();
+        self.prom_requests_in_flight.with_label_values(&[method]).inc();
     }
 
     pub fn record_response(&self, method: &str, status: u16, start: std::time::Instant) {
@@ -86,8 +98,9 @@ impl Metrics {
         let duration = start.elapsed().as_secs_f64();
         self.request_duration.record(duration, attributes);
         self.requests_in_flight.add(-1, attributes);
-        self.prom_request_duration.observe(duration);
-        self.prom_requests_in_flight.dec();
+        // Update Prometheus metrics with labels
+        self.prom_request_duration.with_label_values(&[method, &status.to_string()]).observe(duration);
+        self.prom_requests_in_flight.with_label_values(&[method]).dec();
     }
 }
 
